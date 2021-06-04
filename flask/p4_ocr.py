5b046abf7b3ca7045b77fb633e6450a8695bded4
zeen263 <zeen263@naver.com>
2021-06-04 20:16:05 +0900

    [FEAT] ajax 비동기 업로드 적용, 더 이상 파일을 서버에 저장하지 않음

--- flask/p4_ocr.py
+++ flask/p4_ocr.py
@@ -1,11 +1,12 @@
 
 import os
 import cv2
+import numpy as np
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = "C:\\Users\\l_jad\\Desktop\\OCR_serving\\static\\files"
-LATEX_STRING = "DUMMY"
+LATEX_STRING = " "
 FILE_INFO = ""
 
 app = Flask(__name__)
@@ -17,21 +18,32 @@ def mainPage():
     global LATEX_STRING, FILE_INFO
 
     if request.method == 'POST':
-        file = request.files['img']
+        file = request.files['img'] # werkzeug.datastructrues.FileStorage
         if file:
-            #user_file = secure_filename(file.filename)
+            user_file_name = secure_filename(file.filename)
+            FILE_INFO = user_file_name
+            print(user_file_name)
+
+            filestr = file.read()
+            npimg = np.frombuffer(filestr, np.uint8)
+            img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+
+            '''
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], "user_file.png")) # js 쓰면 파일 저장 안 하고도 전달 가능
 
             img = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], "user_file.png"), cv2.IMREAD_COLOR)
+            '''
             std_img = round(img.std(), 3)
             mean_img = round(img.mean(), 3)
             
             FILE_INFO = "std : " + str(std_img) + ", mean : " + str(mean_img) # 서버 단에서 입력된 이미지를 처리해 돌려주는 게 가능하다는 것을 증명
-            
+
             LATEX_STRING = "$$s^a+m^p+l^e=\\frac{equa}{tion}$$"
             #return jsonify({'LATEX':LATEX_STRING}) # js를 사용해 보자
 
-    return render_template('_main.html', INFO=FILE_INFO, LaTeX=LATEX_STRING)
+    return render_template('main.html', INFO=FILE_INFO, LATEX=LATEX_STRING)
+    # render_template는 새로고침해서 '/' 경로로 요청이 와야 변수를 날릴 수 있나 봐...
+    # ajax 써서 비동기로 날렸으니 이 변수들 전달이 안 됨
 
 
 if __name__ == "__main__":
--- flask/static/js/main.js
+++ flask/static/js/main.js
@@ -1,4 +1,28 @@
-// In progress...
+//
+
+// ====================== thumbnail preview =========================
+function readImage(input) {
+    // 인풋 태그에 파일이 있는 경우
+    if(input.files && input.files[0]) {
+        // 이미지 파일인지 검사 (생략)
+        // FileReader 인스턴스 생성
+        const reader = new FileReader()
+        // 이미지가 로드가 된 경우
+        reader.onload = e => {
+            const previewImage = document.getElementById("preview-image")
+            previewImage.src = e.target.result
+        }
+        // reader가 이미지 읽도록 하기
+        reader.readAsDataURL(input.files[0])
+    }
+}
+// input file에 change 이벤트 부여
+const inputImage = document.getElementById("input-image")
+inputImage.addEventListener("change", e => {
+    readImage(e.target)
+})
+
+// ======================================================
 
 window.onload = function () {
     const userImg = document.querySelector(".user-img")
@@ -6,7 +30,25 @@ window.onload = function () {
     uploader.addEventListener("click", upload)
 }
 
+
+//formData.append('img',document.getElementById('input-image').files[0]);
+
 function upload(e) {
-    e.preventDefault()
+    //e.preventDefault()
+    var formData = new FormData(document.getElementById("upload-form"));
+    formData.append('img',$("#input-image")[0].files[0]);
+
+    $.ajax({
+    url : '/',
+    contentType : false,
+    processData : false,
+    type : 'POST',
+    data : formData,
+    success : function (response) { alert("success")},
+    error : function (response) { alert("fail")},
+    })
 
 }
+
+
+
